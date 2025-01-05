e0229e1661f6f24f209265279ce9a6777beb0056
Collin <cporter.dev@gmail.com>
2025-01-04 22:36:14 -0700

    added some content to navbar & constants folder w/ data

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,15 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return(
-<nav className="border-2 border-blue-500 flexBetween max-container padding-container relative z-30 py-5">
-    <Link href="/">
-        
-    </Link>
-</nav>
-  ) 
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="Application logo" width={75} height={25}/>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
